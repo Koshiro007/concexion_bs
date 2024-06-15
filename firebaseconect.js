6a4c7d0ac967781c70069903be9610533236191b
Alexander Sanchez <shadowhechog@gmail.com>
2024-06-14 19:00:19 -0500

    poryecto_pis

---
+++ firebaseconect.js
@@ -0,0 +1,26 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js';
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA75m7HV31zxlFxd7TED-2kiKM_EiOQ2ew",
+    authDomain: "CalendarAPI.firebaseapp.com",
+    projectId: "CalendarAPI",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"  
+};
+
+const app = initializeApp(firebaseConfig);
+
+const firebase = getFirestore(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { db, storage, analytics, auth };
+
+console.log("Conexión a Firebase establecida correctamente.");
---
+++ formulario.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId:
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const event_form = document.getElementById("event_form");
+const inputX = document.getElementById("X");
+
+event_form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const X = inputX.value;
+
+  try {
+    await addDoc(collection(firestore, "Z"), {
+      Y: X
+    });
+  } catch (error) {
+    console.error("Error al agregar datos", error);
+  }
+});
+
+// Explicacion (X, Y, Z) 
+X hace referencia al "id" (#) del input de entrada de datos
+inputX hace referencia al elemento "input" + el atributo id que posee
+Y hace referencia al nombre de campo (base de datos)
+Z hace referencia al nombre de la colección (base de datos)
